d091f0dfa24fc44621127bc4d2a8716ac4d10ba0
Alexis Candelaria <alexiscandelaria1@gmail.com>
2017-03-20 00:38:56 -0700

    changed url regular expression for taglist to match an optional page number

--- suorganzizer/organizer/urls/tag.py
+++ suorganzizer/organizer/urls/tag.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from ..views import TagList, TagCreate, TagUpdate, TagDelete, tag_detail
 
 urlpatterns = [
-    url(r'^$', TagList.as_view(), name='organizer_tag_list'),
+    url(r'^((?P<page_number>\d+)/)?$', TagList.as_view(), name='organizer_tag_list'),
     url(r'^create/$', TagCreate.as_view(), name='organizer_tag_create'),
     url(r'^(?P<slug>[\w\-]+)/update/$',TagUpdate.as_view(), name='organizer_tag_update'),
     url(r'^(?P<slug>[\w\-]+)/delete/$', TagDelete.as_view(), name='organizer_tag_delete'),
