79e2d0514ef64a4aef3c9a859f351ae79906ecec
Alexis Candelaria <alexiscandelaria1@gmail.com>
2017-03-19 17:23:27 -0700

    added TagList class based view to tag urls

--- suorganzizer/organizer/urls/tag.py
+++ suorganzizer/organizer/urls/tag.py
@@ -1,8 +1,8 @@
 from django.conf.urls import url
-from ..views import tag_list, TagCreate, TagUpdate, TagDelete, tag_detail
+from ..views import TagList, TagCreate, TagUpdate, TagDelete, tag_detail
 
 urlpatterns = [
-    url(r'^$', tag_list, name='organizer_tag_list'),
+    url(r'^$', TagList.as_view(), name='organizer_tag_list'),
     url(r'^create/$', TagCreate.as_view(), name='organizer_tag_create'),
     url(r'^(?P<slug>[\w\-]+)/update/$',TagUpdate.as_view(), name='organizer_tag_update'),
     url(r'^(?P<slug>[\w\-]+)/delete/$', TagDelete.as_view(), name='organizer_tag_delete'),
