5a0fcedc9e02e6916f162fa497b9256fcbf5cfce
Alexis Candelaria <alexiscandelaria1@gmail.com>
2017-03-20 23:17:31 -0700

    added flat pages and sites to installed apps and provided a siteid

--- suorganzizer/suorganzizer/settings.py
+++ suorganzizer/suorganzizer/settings.py
@@ -19,6 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
+SITE_ID = 1
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'a0=0i)ubw9&r#&^m&+8(*$hmm!%xkwr70gh4natr8i_5kwp3qx'
 
@@ -37,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.flatpages',
+    'django.contrib.sites',
     'organizer',
     'blog',
     'contact',
