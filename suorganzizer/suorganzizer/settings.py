c2864bb7f681ce4984b54c0a6e752db942d2509f
Alexis Candelaria <alexiscandelaria1@gmail.com>
2017-03-23 01:06:47 -0700

    added middle ware

--- suorganzizer/suorganzizer/settings.py
+++ suorganzizer/suorganzizer/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.flatpages',
     'django.contrib.sites',
+    'django.contrib.flatpages.middleware.FlatpageFallbackMiddlewware',
     'organizer',
     'blog',
     'contact',
