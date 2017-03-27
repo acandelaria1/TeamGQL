ba80a7a72289160f9b48604845b52dacd3d79138
Alexis Candelaria <alexiscandelaria1@gmail.com>
2017-03-26 17:04:52 -0700

    removed app specific query logic

--- suorganzizer/schema.py
+++ suorganzizer/schema.py
@@ -3,9 +3,6 @@ from graphene import Schema, relay, resolve_only_args
 from graphene_django import DjangoConnectionField, DjangoObjectType
 
 from blog.models import Post as PostModel
-from organizer.models import Tag as TagModel
-from organizer.models import Startup as StartupModel
-from organizer.models import NewsLink as NewsLinkModel
 
 class Post(DjangoObjectType):
     
