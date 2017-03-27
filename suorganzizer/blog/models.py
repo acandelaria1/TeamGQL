9747465b627a91f08383d3159dad72fae33d77a8
Alexis Candelaria <alexiscandelaria1@gmail.com>
2017-03-26 21:56:58 -0700

    revised meta to be upper case

--- suorganzizer/blog/models.py
+++ suorganzizer/blog/models.py
@@ -22,7 +22,6 @@ class Post(models.Model):
         return reverse('blog_post_detail',kwargs={'year':self.pub_date.year,
                                                 'month': self.pub_date.month,
                                                 'slug': self.slug})
-
     def get_update_url(self):
         return reverse('blog_post_update', kwargs={'year': self.pub_date.year, 'month': self.pub_date.month, 'slug': self.slug})
 
